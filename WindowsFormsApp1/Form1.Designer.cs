619598398b2d6c6b5c2137d477b46ab766a2a8ee
sadiaTarin <tarin.cse5.bu@gmail.com>
2022-10-02 19:50:10 +0600

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkbtn = new System.Windows.Forms.Button();
+            this.firstseq = new System.Windows.Forms.TextBox();
+            this.secondseq = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmntTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkbtn
+            // 
+            this.checkbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkbtn.Location = new System.Drawing.Point(67, 382);
+            this.checkbtn.Name = "checkbtn";
+            this.checkbtn.Size = new System.Drawing.Size(151, 53);
+            this.checkbtn.TabIndex = 2;
+            this.checkbtn.Text = "Check";
+            this.checkbtn.UseVisualStyleBackColor = true;
+            this.checkbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // firstseq
+            // 
+            this.firstseq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstseq.Location = new System.Drawing.Point(284, 24);
+            this.firstseq.Multiline = true;
+            this.firstseq.Name = "firstseq";
+            this.firstseq.Size = new System.Drawing.Size(425, 52);
+            this.firstseq.TabIndex = 0;
+            // 
+            // secondseq
+            // 
+            this.secondseq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondseq.Location = new System.Drawing.Point(284, 91);
+            this.secondseq.Multiline = true;
+            this.secondseq.Name = "secondseq";
+            this.secondseq.Size = new System.Drawing.Size(425, 48);
+            this.secondseq.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(284, 196);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(425, 46);
+            this.result.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1st DNA sequence";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(18, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2nd DNA sequence";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(437, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Longest common DNA sequence:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearbtn.Location = new System.Drawing.Point(571, 382);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(138, 53);
+            this.clearbtn.TabIndex = 4;
+            this.clearbtn.Text = "Clear";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(298, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = " Show comment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(61, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Comment";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cmntTxt
+            // 
+            this.cmntTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmntTxt.Location = new System.Drawing.Point(284, 248);
+            this.cmntTxt.Multiline = true;
+            this.cmntTxt.Name = "cmntTxt";
+            this.cmntTxt.Size = new System.Drawing.Size(425, 87);
+            this.cmntTxt.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmntTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.secondseq);
+            this.Controls.Add(this.firstseq);
+            this.Controls.Add(this.checkbtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DNA Sequence Matching";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkbtn;
+        private System.Windows.Forms.TextBox firstseq;
+        private System.Windows.Forms.TextBox secondseq;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clearbtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cmntTxt;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int[,] a;
+        public int[,] b;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String first = firstseq.Text;
+            String second = secondseq.Text;
+            var output = LCS(first, second);
+           result.Text=BackTrack(output, first, second, first.Length, second.Length); //out
+         
+
+            Console.ReadLine();
+
+            int[,] LCS(string s1, string s2)
+            {
+                int[,] c = new int[s1.Length + 1, s2.Length + 1];
+                for (int i = 1; i <= s1.Length; i++)
+                    for (int j = 1; j <= s2.Length; j++)
+                    {
+                        if (s1[i - 1] == s2[j - 1])
+                            c[i, j] = c[i - 1, j - 1] + 1;
+                        else
+                            c[i, j] = c[i - 1, j] > c[i, j - 1] ? c[i - 1, j] : c[i, j - 1];
+                    }
+                return c;
+            }
+
+           string BackTrack(int[,] c, string s1, string s2, int i, int j)
+            {
+                if (i == 0 || j == 0)
+                    return "";
+                else if (s1[i - 1] == s2[j - 1])
+                    return BackTrack(c, s1, s2, i - 1, j - 1) + s1[i - 1];
+                else if (c[i, j - 1] > c[i - 1, j])
+                    return BackTrack(c, s1, s2, i, j - 1);
+                else
+                    return BackTrack(c, s1, s2, i - 1, j);
+            }
+
+
+        }
+
+        private void clearbtn_Click(object sender, EventArgs e)
+        {
+            firstseq.Clear();
+            secondseq.Clear();
+            result.Clear();
+            cmntTxt.Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            String s1 = firstseq.Text;
+            Double s1len = (Double)s1.Length;
+
+            String s3 = result.Text;
+            Double s3len =(Double) s3.Length;
+
+            Double percent = ((s3len/ s1len) * 100.00);
+
+            cmntTxt.Text =percent.ToString();
+             
+           if(percent==0.00)
+            {
+                cmntTxt.Text = "There is no possibility to have a relationship between the DNA sequence holders.The percentage of having relationship is 0%.";
+            }
+
+           else if (percent>0.00 && percent<=30.00)
+           {
+                cmntTxt.Text = "THere is a very low posibility to have a relationship between the DNA sequence holders.The percentage of having relationship is "+percent.ToString()+ "%."; 
+            }
+
+            else if (percent > 30.00 && percent <=60.00)
+            {
+                cmntTxt.Text = "THere is a midium posibility to have a relationship between the DNA sequence holders.The percentage of having relationship is " +percent.ToString() +"%.";
+            }
+
+            else if (percent > 60.00 && percent <= 80.00)
+            {
+                cmntTxt.Text = "THere is a very high posibility to have a relationship between the DNA sequence holders.The percentage of having relationship is " +percent.ToString() + "%";
+            }
+            else
+           {
+                cmntTxt.Text = "WOW!!! That's great....It looks like, both of them have a strong relationship. THere is a very high posibility to have a relationship between the DNA sequence holders.The percentage of having relationship is " +percent.ToString()+ "%.";
+            }
+        }
+    }
+
+}
